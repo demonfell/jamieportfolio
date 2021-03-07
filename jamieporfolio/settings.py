1227be2d015349e29e79a7b421c6d44461232531
James Pemantell <pbin2au@gmail.com>
2021-03-07 08:27:20 -0500

    Set the app to use an environment variable for SECRET_KEY in prod instead of my dev key

--- jamieporfolio/settings.py
+++ jamieporfolio/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-from os import path
+from os import environ, path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7r7%nddg1k!=s-l_02=cd0$r2ahrh8r!usf+6o3txk(lvdo+0c'
+SECRET_KEY = environ.get('SECRET_KEY') or '7r7%nddg1k!=s-l_02=cd0$r2ahrh8r!usf+6o3txk(lvdo+0c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
